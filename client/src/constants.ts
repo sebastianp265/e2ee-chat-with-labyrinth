4d471ccd5802c0309ad4cc35fbd23879f699d63e
Sebastian Pawliński <pawlinskisebastian@gmail.com>
2024-09-16 20:59:41 +0200

    add constants for local storage access
    Took 3 minutes

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -2,6 +2,7 @@
 export const LOGGED_USER_ID_KEY = "logged_user_id"
 export const SESSION_EXPIRES_AT_KEY = "session_expires_at"
 export const DEVICE_KEY = "device"
+export const EPOCH_STORAGE_KEY = "epoch_storage"
 
 // other
 export const SESSION_EXPIRATION_TIME_MIN = 10
